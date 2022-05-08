61cfdd0e776128481adf90053c19be5275d590bd
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-05-07 21:35:56 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/gabrielferreira/br/springapicliente/SpringApiClienteApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.br.springapicliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiClienteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrielferreira/br/springapicliente/SpringApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.br.springapicliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
